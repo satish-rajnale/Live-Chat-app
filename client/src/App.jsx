75ea4cb3cdf08e48b9de97785ebc5115d63050aa
satish-rajnale <satishrajnale98@gmail.com>
2021-04-27 11:58:08 +0530

    scsss config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./app.css";
-import Chat from './Chats';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/app.css";
+import Chat from "./Chats";
 
-
-const App = () => <Chat className="Chat"/>;
+const App = () => (
+                    <div className="Chat">
+                        <div className="userList">
+                            <ul></ul>
+                        </div>
+                        <Chat />
+                    </div>
+                    );
 
 ReactDOM.render(<App />, document.getElementById("app"));
